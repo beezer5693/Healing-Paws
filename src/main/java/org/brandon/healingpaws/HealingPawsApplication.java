9c3fcb38a5e0ed472c293e6224a85a932cca2657
Brandon Bryan <bshwag@gmail.com>
2024-03-11 15:48:31 -0400

    Initial commit

---
+++ src/main/java/org/brandon/healingpaws/HealingPawsApplication.java
@@ -0,0 +1,13 @@
+package org.brandon.healingpaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingPawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingPawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/brandon/healingpaws/HealingPawsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brandon.healingpaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingPawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
