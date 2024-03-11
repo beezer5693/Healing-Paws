24b49a269ac4aef57bb5054e90f60d17231961d8
Brandon Bryan <bshwag@gmail.com>
2024-03-11 16:57:17 -0400

    Added client class with initial fields

---
+++ src/main/java/org/brandon/healingpaws/client/Client.java
@@ -0,0 +1,48 @@
+package org.brandon.healingpaws.client;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clients")
+@EntityListeners(AuditingEntityListener.class)
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "create_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
